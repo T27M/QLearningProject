f12d753dbb4c1bb1e1573fd1e0bacd1b68e000b7
Tom McCracken <3260616+T27M@users.noreply.github.com>
2018-05-14 16:26:03 +0100

    changes to lfa

--- linearfa.py
+++ linearfa.py
@@ -3,40 +3,73 @@ import numpy as np
 import gym
 import sys
 import pickle
-
-actions = [0, 1]
-
-# discount factor
-gamma = 0.95
-
-# decay rate
-decay = 1
-
-# learning rate
-alpha = 0
-
-alpha_zero = 0.2
+import os
+import time
+import json
+import random
 
 
-class QAgent():
+class LfaQAgent():
     def __init__(self):
         # Feature weights
         self.__w = 0.1 * np.ones((2, 4))
+        self.__rewards = []
 
-    def save(self):
-        print('Saving weights...')
+        self.__wrk_dir = './data/lfa/' + time.strftime("%Y%m%d-%H%M%S")
 
-        with open('./data/lfa/lfa.pickle', 'wb') as file:
+        if not os.path.exists(self.__wrk_dir):
+            os.makedirs(self.__wrk_dir)
+
+        self.__reward_path = self.__wrk_dir + '/lfa.reward.json'
+        self.__weights_path = self.__wrk_dir + '/lfa.weights.pickle'
+
+        self.__rewards = []
+
+        # Learning rate
+        self.alpha = 0.8
+
+        self.alpha_zero = 0.8
+        self.alpha_decay = 0.2
+
+        # Discount factor
+        self.gamma = 0.95
+
+        # Random action
+        self.epsilon = 0.1
+
+        self.__actions = [0, 1]
+
+    def paths(self):
+        return self.__wrk_dir
+
+    def add_reward(self, episode, episode_reward):
+        er_dict = {
+            'episode': episode,
+            'episode_reward': episode_reward
+        }
+
+        try:
+            with open(self.__reward_path, 'r') as file:
+                self.__rewards = json.load(file)
+        except:
+            pass
+
+        self.__rewards.append(er_dict)
+
+        with open(self.__reward_path, 'w') as file:
+            json.dump(self.__rewards, file)
+
+    def weights(self):
+        return self.__w
+
+    def save_weights(self):
+        with open(self.__weights_path, 'wb') as file:
             pickle.dump(self.__w, file)
 
-    def load(self):
+    def load_weights(self, path):
         try:
-            with open('./data/lfa/lfa.pickle', 'rb') as file:
+            with open(path + 'lfa.weights.pickle', 'rb') as file:
                 self.__w = pickle.load(file)
-
-            print("Loaded weights..\n")
-            print(self.__w)
-
         except FileNotFoundError:
             print("Weight file not found")
 
@@ -45,43 +78,51 @@ class QAgent():
         return np.asscalar(np.dot(s, self.__w[a]))
 
     def Qs(self, s):
-        return list(map(lambda a: self.Q(s, a), actions))
+        return list(map(lambda a: self.Q(s, a), self.__actions))
 
     def maxQs(self, s):
         return np.max(self.Qs(s))
 
     def predict(self, s):
-        return np.argmax(self.Qs(s))
+        if self.epsilon > random.random():
+            return random.choice(self.__actions)
+        else:
+            return np.argmax(self.Qs(s))
 
     def update_fa(self, s, a, s1, r):
         Qsa = self.Q(s, a)
         maxQ = self.maxQs(s1)
 
         # Weight error
-        error = reward + gamma * maxQ - Qsa
+        error = (reward + self.gamma * maxQ) - Qsa
 
         # Multiply by error
-        s = [(alpha * error) * x for x in s]
+        s = [self.alpha * error * x for x in s]
 
         # Update weights
         self.__w[a] = self.__w[a] + s
 
         # print(self.__w)
 
+    def decay_learning_rate(self, episode):
+        # 1/t decay
+        self.alpha = self.alpha_zero / \
+            (1 + self.alpha_decay * episode)
 
-q = QAgent()
 
-q.load()
+q = LfaQAgent()
+# q.load_weights('./data/lfa/20180514-133021/')
+
+print("Using weights..\n")
+print(q.weights())
 
 env = gym.make('CartPole-v0')
 env.reset()
 
-rewards = []
-
-for i in range(5000):
-
-    print('Episode: ' + str(i))
-    print("Learning Rate:" + str(alpha))
+for episode in range(5000):
+    print('\n')
+    print('Episode: ' + str(episode))
+    print("\tLearning Rate:" + str(q.alpha))
 
     episode_reward = 0
     action = 0
@@ -89,7 +130,7 @@ for i in range(5000):
 
     while(True):
 
-        env.render()
+        # env.render()
 
         # Predict action
         action = q.predict(s)
@@ -97,8 +138,14 @@ for i in range(5000):
         # Take action
         s1, reward, done, info = env.step(action)
 
+        # if done:
+        # reward = -100
+
         # Update QTable
         q.update_fa(s, action, s1, reward)
+
+        # print('\n')
+        # print(q.weights())
 
         # Pass state along
         s = s1
@@ -107,20 +154,20 @@ for i in range(5000):
         episode_reward += reward
 
         if done:
+            print("\tEpisode reward: " + str(episode_reward))
+            q.add_reward(episode, episode_reward)
 
-            print("Episode reward: " + str(episode_reward))
-            rewards.append(episode_reward)
+            print('\tSaving weights...')
+            q.save_weights()
 
-            with open("./data/lfa/reward.txt", "a") as myfile:
-                myfile.writelines("Episode: " + str(i) +
-                                  " | Reward: " + str(episode_reward) + "\n")
+            # if q.alpha > 0.001:
+            #     print('\tDecay learning rate')
+            #     q.decay_learning_rate(episode)
 
-            q.save()
-
-            print("Decaying learning rate..")
-
-            alpha = 1 / (1 + decay * i) * alpha_zero
-
-            print("Current learning rate: " + str(alpha))
-
+            print('\n ---')
             break
+
+print('\n Data saved to: ' + q.paths())
+
+print('\n')
+print(q.weights())
