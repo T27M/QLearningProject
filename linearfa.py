f40adb0b02a4cb6df5a96de4d28b89e02991e30d
Tom McCracken <3260616+T27M@users.noreply.github.com>
2018-05-15 16:18:41 +0100

    Fixed bug where predict would return index instead of action

--- linearfa.py
+++ linearfa.py
@@ -8,11 +8,14 @@ import time
 import json
 import random
 
+from core.featureprocessor import FeatureProcessor
+from core.config import Config
+
 
 class LfaQAgent():
     def __init__(self):
         # Feature weights
-        self.__w = 0.1 * np.ones((2, 4), dtype=np.float64)
+        self.__w = 0.1 * np.ones((4, 163), dtype=np.float64)
         self.__rewards = []
 
         self.__wrk_dir = './data/lfa/' + time.strftime("%Y%m%d-%H%M%S")
@@ -26,11 +29,11 @@ class LfaQAgent():
         self.__rewards = []
 
         # Learning rate
-        self.alpha = 0.4
+        self.alpha = 0.2
         self.__min_learning_rate = 0.01
 
         self.alpha_zero = 0.8
-        self.alpha_decay = 0.01
+        self.alpha_decay = 0.05
 
         # Discount factor
         self.gamma = 0.9
@@ -38,10 +41,22 @@ class LfaQAgent():
         # Random action
         self.epsilon = 0.1
 
-        self.__actions = [0, 1]
+        self.__actions = [1, 2, 3, 4]
 
-        self.__step_decay = 250
+        self.__step_decay = 100
         self.__decay_step_ctr = 0
+
+    def stats(self):
+        print('\n Data saved to: ' + self.paths())
+
+        print('\n Weights:')
+        print(self.weights())
+
+        print('\n Best episode:')
+        print(self.best_episode())
+
+        print('\n Avg score/episode:')
+        print(str(self.avg_score()))
 
     def paths(self):
         return self.__wrk_dir
@@ -56,6 +71,9 @@ class LfaQAgent():
         total = sum(scores)
 
         return total / episodes
+
+    def clear_score(self):
+        self.__rewards = []
 
     def add_reward(self, episode, episode_reward):
         er_dict = {
@@ -90,7 +108,7 @@ class LfaQAgent():
 
     def Q(self, s, a):
         # Q value for state s
-        return np.asscalar(np.dot(s, self.__w[a]))
+        return np.asscalar(np.dot(s, self.__w[a - 1]))
 
     def Qs(self, s):
         return list(map(lambda a: self.Q(s, a), self.__actions))
@@ -102,106 +120,159 @@ class LfaQAgent():
         if self.epsilon > random.random():
             return random.choice(self.__actions)
         else:
-            return np.argmax(self.Qs(s))
+            return self.__actions[np.argmax(self.Qs(s))]
+
+    def act(self, s):
+        return np.argmax(self.Qs(s))
 
     def update_fa(self, s, a, s1, r):
         Qsa = self.Q(s, a)
         maxQ = self.maxQs(s1)
 
         # Weight error
-        error = (reward + self.gamma * maxQ) - Qsa
+        error = (r + self.gamma * maxQ) - Qsa
 
         # Multiply by error
         ms = [self.alpha * error * x for x in s]
 
         # Update weights
-        self.__w[a] = self.__w[a] + ms
+        self.__w[a - 1] = self.__w[a - 1] + ms
 
         # Round weights
-        self.__w[a] = [np.around(x, 3) for x in self.__w[a]]
+        self.__w[a - 1] = [np.around(x, 3) for x in self.__w[a - 1]]
 
         # print(self.__w)
 
     def decay_learning_rate(self, episode):
-        if self.alpha / 2 > self.__min_learning_rate:
-            pass
+        if self.alpha - self.alpha_decay >= self.__min_learning_rate:
+            if self.__decay_step_ctr == self.__step_decay:
+                self.__decay_step_ctr = 0
+                self.alpha = self.alpha - self.alpha_decay
+                return True
+            else:
+                self.__decay_step_ctr = self.__decay_step_ctr + 1
+                return False
 
-        if self.__decay_step_ctr == self.__step_decay:
-            self.__decay_step_ctr = 0
-            self.alpha = self.alpha / 2
-            return True
+                # # 1/t decay
+                # self.alpha = self.alpha_zero / \
+                #     (1 + self.alpha_decay * episode)
         else:
-            self.__decay_step_ctr = self.__decay_step_ctr + 1
-            return False
+            self.alpha = self.__min_learning_rate
 
-            # # 1/t decay
-            # self.alpha = self.alpha_zero / \
-            #     (1 + self.alpha_decay * episode)
 
+class Env():
+    def __init__(self, agent, render, train, decay):
+        self.__render = render
+        self.__train = train
+        self.__decay = decay
+        self.__agent = agent
+
+        if train:
+            print("\tTraining agent...")
+        else:
+            print("\tUsing policy...")
+
+        print('\tUsing weights..\n')
+        print(q.weights())
+
+    def run(self, episodes):
+        # env = gym.make('CartPole-v0')
+        env = gym.make('MsPacman-v0')
+        env.reset()
+
+        config = Config()
+        fp = FeatureProcessor(config)
+
+        for episode in range(episodes):
+            print('\n')
+            print('Episode: ' + str(episode))
+            print("\tLearning Rate:" + str(q.alpha))
+
+            episode_reward = 0
+            action = 1
+            s = env.reset()
+
+            s = fp.extract_features(s)
+
+            while(True):
+                if self.__render:
+                    env.render()
+
+                if train:
+                    # Predict action
+                    action = self.__agent.predict(s)
+                else:
+                    # Use policy
+                    action = self.__agent.act(s)
+
+                print(action)
+
+                # Take action
+                s1, reward, done, _ = env.step(action)
+                s1 = fp.extract_features(s1)
+
+                # if done:
+                # reward = -1000
+
+                if train:
+                    # Update QTable
+                    self.__agent.update_fa(s, action, s1, reward)
+
+                # print('\n')
+                # print(q.weights())
+
+                # Pass state along
+                s = s1
+
+                # Total episode reward
+                episode_reward += reward
+
+                if done:
+                    print("\tEpisode reward: " + str(episode_reward))
+                    self.__agent.add_reward(episode, episode_reward)
+
+                    if self.__train:
+                        print('\tSaving weights...')
+                        self.__agent.save_weights()
+
+                        if self.__decay:
+                            if self.__agent.decay_learning_rate(episode):
+                                print('\tDecay learning rate..')
+
+                    print('\n ---')
+                    break
+
+
+render = False
+train = True
+decay = False
+episodes = 250
 
 q = LfaQAgent()
-# q.load_weights('./data/lfa/20180514-133021/')
+# q.load_weights('./data/lfa/20180515-112554/')
 
-print("Using weights..\n")
-print(q.weights())
+en = Env(q, render, train, decay)
+en.run(episodes)
 
-env = gym.make('CartPole-v0')
-env.reset()
+q.stats()
 
-for episode in range(1000):
-    print('\n')
-    print('Episode: ' + str(episode))
-    print("\tLearning Rate:" + str(q.alpha))
+while True:
+    key = ""
+    while key.lower() not in ['r', 'rr', 't', 'q']:
+        key = input(
+            '(r: run the policy)(rr:run the policy and render(NOTE: this is very slow!))(t: train again with same settings)(q: quit):')
 
-    episode_reward = 0
-    action = 0
-    s = env.reset()
+    if key.lower() == "r":
+        train = False
 
-    while(True):
+    if key.lower() == 'rr':
+        train = False
+        render = True
 
-        # env.render()
+    if key.lower() == 'q':
+        break
 
-        # Predict action
-        action = q.predict(s)
-
-        # Take action
-        s1, reward, done, info = env.step(action)
-
-        # if done:
-        # reward = -1000
-
-        # Update QTable
-        q.update_fa(s, action, s1, reward)
-
-        # print('\n')
-        # print(q.weights())
-
-        # Pass state along
-        s = s1
-
-        # Total episode reward
-        episode_reward += reward
-
-        if done:
-            print("\tEpisode reward: " + str(episode_reward))
-            q.add_reward(episode, episode_reward)
-
-            print('\tSaving weights...')
-            q.save_weights()
-
-            if q.decay_learning_rate(episode):
-                print('\tDecay learning rate..')
-
-            print('\n ---')
-            break
-
-print('\n Data saved to: ' + q.paths())
-
-print('\n Weights:')
-print(q.weights())
-
-print('\n Best episode:')
-print(q.best_episode())
-
-print('\n Avg score/episode:')
-print(str(q.avg_score()))
+    q.clear_score()
+    en = Env(q, render, train, decay)
+    en.run(episodes)
+    q.stats()
